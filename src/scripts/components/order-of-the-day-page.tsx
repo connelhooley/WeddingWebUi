4abed24e6a34077220cf052e627cfcb688bffe1d
Connel Hooley <connelhooley@gmail.com>
2019-02-28 21:17:09 +0000

    Basic order of the day

--- src/scripts/components/order-of-the-day-page.tsx
+++ src/scripts/components/order-of-the-day-page.tsx
@@ -6,8 +6,48 @@ export function OrderOfTheDayPage(): JSX.Element {
     return (
         <div id="order-of-the-day-page">
             <h1>Order Of The Day</h1>
-            <p>More information coming soon</p>
-            <img src={bannerImg} />
+            <table>
+                <tr>
+                    <td className="time">12.00pm</td>
+                    <td className="description">Wedding ceremony</td>
+                </tr>
+                <tr>
+                    <td className="time">12.40pm</td>
+                    <td className="description">Welcome drinks and canapes</td>
+                </tr>
+                <tr>
+                    <td className="time">3.00pm</td>
+                    <td className="description">All guests to be seated</td>
+                </tr>
+                <tr>
+                    <td className="time">3.15pm</td>
+                    <td className="description">Speeches </td>
+                </tr>
+                <tr>
+                    <td className="time">3.45pm</td>
+                    <td className="description">Wedding breakfast</td>
+                </tr>
+                <tr>
+                    <td className="time">6.30pm</td>
+                    <td className="description">Glitter bar (until 8.30pm)</td>
+                </tr>
+                <tr>
+                    <td className="time">7.30pm</td>
+                    <td className="description">Evening guests arrive</td>
+                </tr>
+                <tr>
+                    <td className="time">7.45pm</td>
+                    <td className="description">Cutting of the cake followed by first dance</td>
+                </tr>
+                <tr>
+                    <td className="time">8.30pm</td>
+                    <td className="description">Evening BBQ and cake to be shared</td>
+                </tr>
+                <tr>
+                    <td className="time">10.15pm</td>
+                    <td className="description">Fireworks</td>
+                </tr>
+            </table>
         </div>
     );
 }

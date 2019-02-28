305e95d6ce5465bec520f380dbc33de77acf265a
Connel Hooley <connelhooley@gmail.com>
2019-02-28 21:20:23 +0000

    Tweaked order of day

--- src/scripts/components/order-of-the-day-page.tsx
+++ src/scripts/components/order-of-the-day-page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import bannerImg from "../../images/daisy-chain-ds-h.png";
-
 export function OrderOfTheDayPage(): JSX.Element {
     return (
         <div id="order-of-the-day-page">
